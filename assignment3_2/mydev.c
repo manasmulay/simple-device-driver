738880b99b7991397f728e6d1c749a520ed293e5
Manas Mulay <manasmulay@gmail.com>
2019-04-26 22:13:51 +0530

    Q2 to debug

--- assignment3_2/mydev.c
+++ assignment3_2/mydev.c
@@ -1,33 +1,71 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/fs.h>
 
-static char device_buf[100] = "";
+static char dev_buf[100] = "";
+const char *dev_name = "mydev";
 
-int register_chrdev(unsigned int major, const char *name, struct file_operations *fops);
-int unregister_chrdev(unsigned int major, const char*name);
+static uint32_t open_instances = 0;
 
-ssize_t mydev_read(struct file *fileptr, char *user_buffer, size_t len, loff_t *off){
-	
-}
-ssize_t mydev_write (struct file *, const char *user_buff, size_t loff_t *);
-int mydev_open (struct inode *, struct file *);
-int mydev_close(struct inode *, struct file *);
+// Function Declarations
+int mydev_open(struct inode *inodep, struct file *fileptr);
+ssize_t mydev_read(struct file *fileptr, char *user_buff, size_t len, loff_t *off);
+ssize_t mydev_write(struct file *fileptr, const char *user_buff, size_t len, loff_t *off);
+int mydev_close(struct inode *inodeptr, struct file *fileptr);
+
+struct file_ops fops ={
+	.read = mydev_read,
+	.write = mydev_write,
+	.open = mydev_open,
+	.release = mydev_close
+};
 
 static int __init mydev_init(void){
-	register_chrdev();
+	register_chrdev(250, dev_name, &fops);
+	printk(KERN_INFO "Device %s connected\n", dev_name);
 	return 0;
 }
 
-
 static void __exit mydev_exit(void){
-	unregister_chrdev();
+	unregister_chrdev(250, dev_name);
+	printk(KERN_INFO "Device %s disconnected\n", dev_name);
 }
 
-struct file_ops fops ={
-	.read = mydevice_read,
-	.open = mydevice_open,
-	.release = mydevice_close,
-	.write = mydevice_write,
-};
+int mydev_open (struct inode *inodep, struct file *fileptr){
+	printk(KERN_INFO "Device file opened for %s", dev_name);
+	open_instances += 1;
+}
+
+ssize_t mydev_read(struct file *fileptr, char *user_buff, size_t len, loff_t *off){
+	ssize_t count = 0;
+	while(dev_buf[count] != '\0')
+	{
+		put_user(dev_buf[count], user_buff++);
+		count++;
+	}
+	put_user("\0", user_buff++);
+	return count;
+}
+
+ssize_t mydev_write(struct file *fileptr, const char *user_buff, size_t len, loff_t *off){
+	ssize_t count = 0;
+	size_t written = len - 1;
+	memset(dev_buf, 0, 100);
+	while(written >= 0){
+		dev_buf[written] = user_buff[count];
+		count++;
+		written--;
+	}
+	dev_buf[len] = "\0";
+	return count;
+}
+
+int mydev_close(struct inode *inodeptr, struct file *fileptr)
+{
+	count--;
+	printk(KERN_INFO "Device file closed for %s", dev_name);
+	return count;
+}
+
 module_init(mydev_init);
 module_exit(mydev_exit);
