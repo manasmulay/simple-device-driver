e7014b2b33cbeb46b03db6220f0748bd00489df9
Manas Mulay <manasmulay@gmail.com>
2019-04-28 20:02:30 +0530

    String argv added

--- assignment3_2/test_device.c
+++ assignment3_2/test_device.c
@@ -7,12 +7,12 @@
 
 int main(int argc, char const *argv[])
 {
-	// assert(argc > 1);
+	assert(argc > 1);
 	char buf[50] = "abcd";
 	int i = 0;
 	int fp = open("/dev/mydevice_driver", O_RDWR);
-	printf("%s\n", buf);
-	write(fp, &buf, strlen(buf));
+	printf("%s\n", argv[1]);
+	write(fp, argv[1], strlen(argv[1]));
 	int x = 5;
 	memset(buf,0,50);
 	read(fp, &buf, 50);
